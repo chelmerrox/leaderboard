6c9e634234843a7937ea95de567c0086f87f4f2c
Losalini <c.rokocakau@gmail.com>
2022-09-01 06:36:32 +1200

    Refactor addScore() function

--- src/index.js
+++ src/index.js
@@ -2,15 +2,8 @@ import './style.css';
 import { getAllScores, addScore } from './modules/api-methods.js';
 
 const form = document.querySelector('.form');
-const userNameField = document.querySelector('.user-name-field');
-const userScoreField = document.querySelector('.user-score-field');
-export let userNameVal;
-export let userScoreVal;
 const refreshBtn = document.querySelector('.refresh-btn');
 export const recentScoresContainer = document.querySelector('.recent-scores-container');
-
-// Endpoint with the id of my game: A game
-const leaderboardAPIURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/sZFhupVBKPQG0DoGJb3U/';
 
 getAllScores();
 
@@ -25,10 +18,10 @@ refreshBtn.addEventListener('click', (e) => {
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  userNameVal = userNameField.value;
-  userScoreVal = userScoreField.value;
+  const userNameVal = document.querySelector('.user-name-field').value;
+  const userScoreVal = document.querySelector('.user-score-field').value;
 
-  addScore();
+  addScore(userNameVal, userScoreVal);
 
   form.reset();
 });
--- src/modules/api-methods.js
+++ src/modules/api-methods.js
@@ -1,48 +1,48 @@
-import { recentScoresContainer, userNameVal, userScoreVal } from './../index.js';
+import { recentScoresContainer } from '../index.js';
 
 // Display the name & scores on the site
 const displayScores = (data) => {
   const recentScoreDiv = document.createElement('div');
   recentScoreDiv.classList.add('recent-score');
 
-  data.forEach((userdata, i) => {
+  data.forEach((userdata) => {
     recentScoreDiv.innerHTML += `<p>${userdata.user}: ${userdata.score}</p>`;
 
     recentScoresContainer.appendChild(recentScoreDiv);
   });
 
   const pArray = Array.from(document.querySelectorAll('p'));
-  
+
   pArray.forEach((element, j) => {
-    if (j%2 === 0){
+    if (j % 2 === 0) {
       element.classList.add('grey');
     } else {
       element.classList.remove('grey');
     }
-  })
+  });
 };
 
 // GET method (for Get all scores)
 export const getAllScores = async () => {
   const options = {
-    method: "GET",
-    headers: { "Content-type": "application/json; charset=UTF-8"},
+    method: 'GET',
+    headers: { 'Content-type': 'application/json; charset=UTF-8' },
   };
 
   fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/sZFhupVBKPQG0DoGJb3U/scores/', options)
     .then((response) => response.json())
     .then((data) => displayScores(data.result));
-}
+};
 
 // POST method (Create)
-export const addScore = async () => {
+export const addScore = async (userNameVal, userScoreVal) => {
   const options = {
-    method: "POST",
-    headers: { "Content-type": "application/json; charset=UTF-8"},
-    body: JSON.stringify({ 
+    method: 'POST',
+    headers: { 'Content-type': 'application/json; charset=UTF-8' },
+    body: JSON.stringify({
       user: userNameVal,
-      score: userScoreVal
-    })
+      score: userScoreVal,
+    }),
   };
 
   const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/sZFhupVBKPQG0DoGJb3U/scores/', options);
