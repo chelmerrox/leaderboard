449172342b19d1c52007a59a98e3ba2c0e0ead5d
Losalini <c.rokocakau@gmail.com>
2022-09-01 06:13:37 +1200

    Create getAllScores() function for refresh button

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './style.css';
+import { getAllScores } from './modules/api-methods.js';
+
+const form = document.querySelector('.form');
+const userNameField = document.querySelector('.user-name-field');
+const userScoreField = document.querySelector('.user-score-field');
+let userNameVal;
+let userScoreVal;
+const refreshBtn = document.querySelector('.refresh-btn');
+export const recentScoresContainer = document.querySelector('.recent-scores-container');
+
+// Endpoint with the id of my game: A game
+const leaderboardAPIURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/sZFhupVBKPQG0DoGJb3U/';
+
+getAllScores();
+
+refreshBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  recentScoresContainer.innerHTML = '';
+
+  getAllScores();
+});
---
+++ src/modules/api-methods.js
@@ -0,0 +1,35 @@
+import { recentScoresContainer } from './../index.js';
+
+// Display the name & scores on the site
+const displayScores = (data) => {
+  const recentScoreDiv = document.createElement('div');
+  recentScoreDiv.classList.add('recent-score');
+
+  data.forEach((userdata, i) => {
+    recentScoreDiv.innerHTML += `<p>${userdata.user}: ${userdata.score}</p>`;
+
+    recentScoresContainer.appendChild(recentScoreDiv);
+  });
+
+  const pArray = Array.from(document.querySelectorAll('p'));
+  
+  pArray.forEach((element, j) => {
+    if (j%2 === 0){
+      element.classList.add('grey');
+    } else {
+      element.classList.remove('grey');
+    }
+  })
+};
+
+// GET method (for Get all scores)
+export const getAllScores = async () => {
+  const options = {
+    method: "GET",
+    headers: { "Content-type": "application/json; charset=UTF-8"},
+  };
+
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/sZFhupVBKPQG0DoGJb3U/scores/', options)
+    .then((response) => response.json())
+    .then((data) => displayScores(data.result));
+}
